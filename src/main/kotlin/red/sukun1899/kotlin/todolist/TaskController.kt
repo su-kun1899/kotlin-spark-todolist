b6516f2146fcd74f753740acf9a3e24ed9bcc5bf
su-kun1899 <higedrum.coz@gmail.com>
2018-03-28 01:05:11 +0900

    タスクの保管

--- src/main/kotlin/red/sukun1899/kotlin/todolist/TaskController.kt
+++ src/main/kotlin/red/sukun1899/kotlin/todolist/TaskController.kt
@@ -1,19 +1,13 @@
 package red.sukun1899.kotlin.todolist
 
-import spark.Request
-import spark.Response
 import spark.Route
 
 /**
  * @author su-kun1899
  */
-class TaskController {
+class TaskController(private val taskRepository: TaskRepository) {
 
     fun index(): Route = Route { request, response ->
-        // ダミーデータを返す
-        listOf(
-                Task(1, "クリーニングに出す", false),
-                Task(2, "住民票を取得する", true)
-        )
+        taskRepository.findAll()
     }
 }
